7410447c5113755ff7eed1ff5e0eef8afe8070bb
Sumedh Ghude <sumedh.ghude@gmail.com>
2020-12-16 12:29:11 +0530

    just a commit before i implement recursion limits during reflection calculations

--- RayTracerChallenge/Main.cpp
+++ RayTracerChallenge/Main.cpp
@@ -41,40 +41,34 @@ void prepareThread(std::vector<Color>& colorArray, const Camera& camera, World& 
 int getLowerLimit(int currentIndex, int height);
 int getUpperLimit(int currentIndex, int maxIndex, int height);
 
-/*
 int main() {
 
-	StripePattern* sPat1 = new StripePattern(Matrix::createIdentityMatrix(4), Color(0.3f, 0.9f, 0.3f), Color(1.0f, 1.0f, 1.0f));
-	StripePattern* sPat2 = new StripePattern(Matrix::createRotationMatrix('y', constants::gPI / 2.0f, true), Color(0.3f, 0.9f, 0.3f), Color(0.9f, 0.9f, 0.9f));
-	BlendedPattern* bPat = new BlendedPattern(Matrix::createIdentityMatrix(4), sPat1, sPat2);
-	StripePattern* sPat = new StripePattern(Matrix::createScalingMatrix(0.3f, 0.3f, 0.3f), Color(1.0f, 0.0f, 0.0f), Color(0.0f, 0.0f, 1.0f));
-
-	Material materialPlane(bPat, 0.1f, 0.7f, 0.3f, 100.0f, 0.0f);
-	Material materialBall(sPat, 0.9f, 0.8f, 0.2f, 250.0f, 0.0f);
+	Material materialPlane(Color(0.0f, 1.0f, 0.0f), 1.0f, 0.7f, 0.3f, 100.0f, 0.9f);
+	Material materialBall(Color(1.0f, 0.0f, 0.0f), 1.0f, 0.8f, 0.2f, 250.0f, 0.9f);
 
 	std::vector<Hitable*> objectList;
 	objectList.push_back(new Plane(1, Matrix::createTranslationMatrix(0.0f, -1.0f, 0.0f), materialPlane));
-	objectList.push_back(new Sphere(2, Matrix::createIdentityMatrix(4), materialBall));
+	objectList.push_back(new Plane(2, Matrix::createTranslationMatrix(0.0f, 4.0f, 0.0f), materialBall));
 
 	std::vector<PointLight> lightList;
 	lightList.push_back(PointLight(Point(7.0f, 5.0f, 3.0f), Color(1.0f, 1.0f, 1.0f)));
-	lightList.push_back(PointLight(Point(2.0f, 4.0f, 2.0f), Color(1.0f, 1.0f, 1.0f)));
 
 	//Generates a World object
 	World world(objectList, lightList);
 
 	float _1ByRoot2 = 1.0f / sqrt(2);
 
-	Ray rayIn(Point(0.0f, 0.0f, 3.0f), Vector(0, -_1ByRoot2, -_1ByRoot2));
+	Ray rayIn(Point(0.0f, 1.0f, -1.0f), Vector(0, -_1ByRoot2, _1ByRoot2));
 	Intersection intersection(sqrt(2),objectList[0]);
 	IntersectionComputations comps(rayIn, intersection);
+	Intersections temp;
+	IntersectionComputations temp1;
+	//WTF is going on here ???? Fix it!!!
 	comps.prepareComputations(rayIn, intersection);
-	std::cout << PointLight::getReflectedColor(world, comps) << '\n';
+	std::cout << PointLight::getColorAt(rayIn, world, temp, temp1) << '\n';
 
 	return 0;
 }
-
-*/
 
 //I'll give a quick rundown of how to use this thing
 //I'm planning on adding a better system of doing this rather than changing main()
@@ -82,13 +76,14 @@ int main() {
 
 //Firstly, go to "Constants.h" to change some of the constants associated with the Ray-Tracing engine
 
+/*
 //Now we begin the actual main() function
 int main() {
 	//This Ray-Tracer uses a Right-handed coordinate system:
 	//X-axis is to the Right, Y-axis is vertically Upwards, and Z-axis is the cross product
 
-	Point cameraFrom(0.0f, 0.5f, 4.0f);
-	Point cameraTo(0.0f, 0.5f, 0.0f);
+	Point cameraFrom(6.0f, 0.5f, 6.0f);
+	Point cameraTo(0.0f, 3.5f, 0.0f);
 	Vector cameraUpVector(0.0f, 1.0f, 0.0f);
 	Camera camera(constants::gWidth, constants::gHeight, (constants::gPI / 2.0f), Matrix::createViewTransformationMatrix(cameraTo, cameraFrom, cameraUpVector));
 
@@ -98,17 +93,19 @@ int main() {
 	StripePattern* sPat = new StripePattern(Matrix::createScalingMatrix(0.3f, 0.3f, 0.3f), Color(1.0f, 0.0f, 0.0f), Color(0.0f, 0.0f, 1.0f));
 
 
-	Material materialPlane(bPat, 0.1f, 0.7f, 0.3f, 100.0f, 0.0f);
+	Material materialPlane(bPat, 0.1f, 0.7f, 0.3f, 100.0f, 0.7f);
 	Material materialBall(sPat, 0.1f, 0.8f, 0.2f, 250.0f, 0.0f);
 	
 	std::vector<Hitable*> objectList;
-	objectList.push_back(new Plane(1, Matrix::createRotationMatrix('x', constants::gPI / 4.0f, false), materialPlane));
-	objectList.push_back(new Sphere(2, Matrix::createTranslationMatrix(0.0f,0.3f,0.0f), materialBall));
+	objectList.push_back(new Plane(1, Matrix::createRotationMatrix('x', constants::gPI / 2.0f, true), materialPlane));
+	objectList.push_back(new Plane(2, Matrix::createRotationMatrix('z', constants::gPI / 2.0f, true), materialPlane));
+	objectList.push_back(new Plane(3, Matrix::createIdentityMatrix(4), materialPlane));
+	objectList.push_back(new Sphere(4, Matrix::createTranslationMatrix(2.0f,2.5f,2.0f), materialBall));
 
 	std::vector<PointLight> lightList;
-	lightList.push_back(PointLight(Point(7.0f, 5.0f, 3.0f), Color(1.0f, 1.0f, 1.0f)));
-	lightList.push_back(PointLight(Point(2.0f, 4.0f, 2.0f), Color(1.0f, 1.0f, 1.0f)));
-
+	lightList.push_back(PointLight(Point(2.0f, 7.0f, 2.0f), Color(1.0f, 1.0f, 1.0f)));
+	lightList.push_back(PointLight(Point(5.0f, 1.5f, 5.0f), Color(1.0f, 1.0f, 1.0f)));
+	
 	//Generates a World object
 	World world(objectList, lightList);
 
@@ -158,6 +155,7 @@ int main() {
 
 	return 0;
 }
+*/
 
 Ray getRayForPixel(int xPixel, int yPixel, const Camera& camera) {
 	//The x and y offset values that point to the point in space that represents the center of the pixel we want to draw a ray to
@@ -188,14 +186,9 @@ void prepareThread(std::vector<Color>& colorArray, const Camera& camera, World& 
 			Intersections intersections;
 			IntersectionComputations computations;
 
-			if (world.hit(tempRay, intersections, computations)) {
-				//std::cout << "World hit!\n";
-				computations.prepareComputations(tempRay, intersections.m_intersections[intersections.m_firstIntersectionIndex]);
-				colorArray.push_back(PointLight::getLighting(world, computations));
-			}
-			else {
-				colorArray.push_back(Color(0.0f, 0.0f, 0.0f));
-			}
+
+
+			colorArray.push_back(PointLight::getColorAt(tempRay, world, intersections, computations));
 		}
 	}
 }
